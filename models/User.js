2df9fd0a041c84aeb72731d6c7518a918e80c3c1
Evgeniy <Newet777@gmail.com>
2024-08-02 12:48:00 +0300

    Mongo Schema

--- models/User.js
+++ models/User.js
@@ -1,28 +1,28 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const privatePaths = require('mongoose-private-paths')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const privatePaths = require("mongoose-private-paths");
 
 const userSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
     required: true,
-    private: true
+    private: true,
   },
   createdDate: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
-userSchema.plugin(privatePaths)
+userSchema.plugin(privatePaths);
 
-module.exports = mongoose.model('users', userSchema)
+module.exports = mongoose.model("users", userSchema);
