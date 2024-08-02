a42c3ee547f3caea80e55b6e0acf60fabe26baa6
Evgeniy <Newet777@gmail.com>
2024-08-02 12:32:44 +0300

    Mongo support

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+require('dotenv').config()
+
 const Koa = require("koa");
 
 const config = require("./lib/config");
 const handlers = require("./handlers");
+const mongooseConfig = require('./lib/mongoose-config')
 
 const app = new Koa();
 
 handlers.forEach((h) => app.use(h));
 
 module.exports = (callback) => {
+  mongooseConfig();
   app.listen(config.port, callback);
   return app;
 };
