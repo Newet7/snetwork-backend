8040c7a3ef3f7009c2db2d071d5cf966d4d61438
Evgeniy <Newet777@gmail.com>
2024-08-09 18:48:46 +0300

    add controllers

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const Router = require("koa-router");
+
+const auth = require("./auth");
+const users = require("./users");
+
+const router = new Router().prefix("/api");
+
+router.use(auth, users);
+
+module.exports = router;
